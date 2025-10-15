ff0b44fb20f10f6cd989dd8cf34a48dc77dd8e43
Ernais <tonybno@yahoo.fr>
2025-10-15 22:28:40 +0200

    feat: New button appointment in home page

--- src/components/MainHome.jsx
+++ src/components/MainHome.jsx
@@ -1,28 +1,33 @@
 import React from "react";
-import imageMains from "../assets/Image/Accueil/imageaccueil.png";
+import imageHand from "../assets/Image/Accueil/imageaccueil.png";
 import "../style/components/MainHome.scss";
 
 function MainHome() {
   return (
     <div className="mainHome">
-      <div className="divMains">
+      <div className="divHand">
         <img
-          className="divMains__image"
-          src={imageMains}
-          alt="Mains qui se tiennent"
+          className="divHand__image"
+          src={imageHand}
+          alt="Mains qui se touchent"
         />
       </div>
       <div className="titles">
         <h1 className="titles__first">DU REGARD A LA MANUCURE</h1>
         <h3 className="titles__second">
-          DES MAINS PARFAITES, UN REGARD <br/>ENVOÛTANT, UNE SEULE ADRESSE
+          DES MAINS PARFAITES, UN REGARD <br />
+          ENVOÛTANT, UNE SEULE ADRESSE !
         </h3>
         <h3 className="titles__three">
-          SUBLIMEZ VOS CILS ET <br/>SOURCILS AVEC DES SOINS DE <br/>PRECISIONS
+          SUBLIMEZ VOS CILS ET <br />
+          SOURCILS AVEC DES SOINS DE <br />
+          PRECISIONS
         </h3>
       </div>
-      <div className="buttonAppointment">
-        <button>Prenez rendez-vous !</button>
+      <div className="buttonHand">
+        <button className="buttonHand__Appointment">
+          Prendre rendez-vous !
+        </button>
       </div>
     </div>
   );
